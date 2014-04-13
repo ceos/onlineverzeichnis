54a8f9b65e3c17194769c06f2516fdc179dab51e
osman <osman@localhost>
2014-04-13 22:39:08 +0200

    initial version

---
+++ src/main/java/onlineverzeichnis/controller/VerzeichnisManagedBean.java
@@ -0,0 +1,22 @@
+package onlineverzeichnis.controller;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+@ManagedBean
+@SessionScoped
+public class VerzeichnisManagedBean {
+
+	/**
+	 * Creates a new instance of VerzeichnisManagedBean
+	 */
+	public VerzeichnisManagedBean() {
+	}
+
+	private String welcomeText = "Onlineverzeichnis Neu";
+
+	public String getWelcomeText() {
+		return welcomeText;
+	}
+
+}
